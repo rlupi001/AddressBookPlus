e54440b7e92e0b1c52a82ce2959107e24f5a7413
ripley vit lupine <rlupi001@bespin.cs.fiu.edu>
2014-04-05 14:26:45 -0400

    Initial Commit

---
+++ AddressPlus_Ripley_Lupine/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AddressPlus_Ripley_Lupine
+//
+//  Created by ripley vit lupine on 4/5/14.
+//  Copyright (c) 2014 ripley vit lupine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
